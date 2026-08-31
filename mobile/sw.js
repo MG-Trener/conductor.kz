a06647a9ad566e2c99927aab4d605380189a1d9c
MG-Trener <mihagavr@gmail.com>
2026-08-31 17:38:56 +0500

    Bump PWA cache for inventory state fix

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "conductor-mobile-v15";
+const CACHE = "conductor-mobile-v16";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const APP_SHELL = [
   "./warehouse.css?v=15",
   "./app.js?v=15",
   "./firebase-config.js?v=15",
+  "./inventory-state.js?v=16",
   "./firestore-error-help.js?v=15",
   "./manifest.webmanifest?v=15",
   "./icon.svg"
