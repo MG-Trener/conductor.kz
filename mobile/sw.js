3cda7883d758004da25bbe5b0e1c37327e517bb2
MG-Trener <mihagavr@gmail.com>
2026-08-31 13:33:32 +0500

    Refresh PWA cache for inventory accounting

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "conductor-mobile-v7";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./leads.js", "./lead-to-sale.js", "./customers.js", "./auth-config-fix.js", "./auth-tools.js", "./firebase-config.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "conductor-mobile-v8";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./leads.js", "./lead-to-sale.js", "./customers.js", "./inventory.js", "./auth-config-fix.js", "./auth-tools.js", "./firebase-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
