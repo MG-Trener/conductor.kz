686f07c2b619882c8c4187c92a6169f351f7b900
MG-Trener <mihagavr@gmail.com>
2026-09-01 18:22:15 +0500

    Keep warehouse UI below Android status bar

--- android-app/MainActivity.template.java
+++ android-app/MainActivity.template.java
@@ -1,12 +1,35 @@
 package __PACKAGE_NAME__;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setStatusBarColor(Color.rgb(7, 10, 18));
+        getWindow().setNavigationBarColor(Color.rgb(7, 10, 18));
         super.onCreate(savedInstanceState);
+        applySystemBarInsets();
+    }
+
+    private void applySystemBarInsets() {
+        final View webView = getBridge() != null ? getBridge().getWebView() : null;
+        if (webView == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
+
+        webView.setOnApplyWindowInsetsListener((view, insets) -> {
+            final int statusBarTop = insets.getSystemWindowInsetTop();
+            view.setPadding(
+                view.getPaddingLeft(),
+                statusBarTop,
+                view.getPaddingRight(),
+                view.getPaddingBottom()
+            );
+            return insets;
+        });
+        webView.requestApplyInsets();
     }
 }
